416edef81b6992aef03c86ae47d1dd0ce094c99c
malvaro71 <55465735+malvaro71@users.noreply.github.com>
2023-12-12 14:58:59 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -88,3 +88,32 @@ document.getElementById("yCoordinate").max = canvas.height / 2;
 //Call the function when some value is entered
 document.getElementById("xCoordinate").oninput = function () { updateMagnitudeAndDirection() };
 document.getElementById("yCoordinate").oninput = function () { updateMagnitudeAndDirection() };
+
+function changeLanguage(languageCode) {
+    Array.from(document.getElementsByClassName('lang')).forEach(function (elem) {
+        if (elem.classList.contains('lang-' + languageCode)) {
+             elem.style.display = 'initial';
+        }
+        else {
+             elem.style.display = 'none';
+        }
+    });
+}
+
+// select handler
+const selector = document.getElementById('langSelector');
+selector.addEventListener('change', function (evt) {
+    changeLanguage(this.value);
+});
+
+// detect initial browser language
+const lang = navigator.userLanguage || navigator.language || 'en-EN';
+const startLang = Array.from(selector.options).map(opt => opt.value).find(val => lang.includes(val)) || 'en';
+changeLanguage(startLang);
+
+// updating select with start value
+selector.selectedIndex = Array.from(selector.options).map(opt => opt.value).indexOf(startLang)
+
+// fill "The selected language is:"
+document.getElementById('browserLang').innerText = lang;
+document.getElementById('startLang').innerText = startLang;
