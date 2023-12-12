c2e4fc58da7c8019c06f490ad6e10e0c954cce30
malvaro71 <55465735+malvaro71@users.noreply.github.com>
2023-12-12 17:38:21 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -92,7 +92,7 @@ document.getElementById("yCoordinate").oninput = function () { updateMagnitudeAn
 function changeLanguage(languageCode) {
     Array.from(document.getElementsByClassName('lang')).forEach(function (elem) {
         if (elem.classList.contains('lang-' + languageCode)) {
-             elem.style.display = ""; //'initial'; test
+             elem.style.display = 'initial';
         }
         else {
              elem.style.display = 'none';
