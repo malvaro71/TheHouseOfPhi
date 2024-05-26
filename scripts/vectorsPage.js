16c849cce1441ca788821c9d52f52fdb06304a1c
Malvaro <malvaro71@hotmail.com>
2024-05-26 18:07:43 +0200

    a

--- scripts/vectorsPage.js
+++ scripts/vectorsPage.js
@@ -380,12 +380,10 @@
 	myPlane2_2.drawAxes("y-axis", "x-axis", "O");
 
 	// Draw horizontal and vertical lines to better show plane coordinates
-	let x;
-    for (x = xMin; x <= xMax; x += 1) {
+    for (let x = xMin; x <= xMax; x += 1) {
         myPlane2_2.drawSegment([x, yMin], [x, yMax], {strokeColor: "green", strokeDasharray: "5,5", strokeWidth: 1});
     }
-	let y;
-	for (y = yMin; y <= yMax; y += 1) {
+	for (let y = yMin; y <= yMax; y += 1) {
         myPlane2_2.drawSegment([xMin, y], [xMax, y], {strokeColor: "green", strokeDasharray: "5,5", strokeWidth: 1});
     }
 
@@ -399,8 +397,7 @@
 	let initialPoint = [0, 0];
 	const vectorList = [vectorA, vectorB, vectorC, vectorD];
 	const vectorNames = ["a", "b", "c", "d"];
-	let v;
-	for(v = 0; v < vectorList.length; v += 1){
+	for(let v = 0; v < vectorList.length; v += 1){
 		myPlane2_2.drawVector(initialPoint, vectorList[v], {}, {textContent: vectorNames[v]});
 		initialPoint = add(initialPoint, vectorList[v]);
 	}
