cb4af7bbd4a0c63a09462f0d736b23a1165e45e8
malvaro71 <55465735+malvaro71@users.noreply.github.com>
2025-07-01 17:01:04 +0200

    Update KinematicsPage.js

--- scripts/KinematicsPage.js
+++ scripts/KinematicsPage.js
@@ -92,9 +92,9 @@
     // set a cartesian plane
 	const myPlane1_3 = new CartesianPlane(svg1_3, 0, 8, 0, 8);
 
-	// set a function to generate a lis of points: f(x) = 5 - (x^2)/100
+	// set a function to generate a lis of points: f(x) = 5 - (x^2)/40
 	function f1_3(x) {
-	    return 5 - (x * x)/50;
+	    return 5 - (x * x)/40;
 	}
     
     // set a list of points that define the path of movement. 
