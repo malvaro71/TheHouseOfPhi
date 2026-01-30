3d0846fd0e09889523168cb260d041a691004978
Malvaro <malvaro71@hotmail.com>
2026-01-30 18:47:00 +0100

    start migration from html to mdx

--- empty/astro.config.mjs
+++ empty/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx({
+    // Habilita la sintaxis de LaTeX ($...$)
+    remarkPlugins: [remarkMath],
+    // Renderiza el LaTeX a HTML y SVG usando KaTeX
+    rehypePlugins: [rehypeKatex]
+  })]
+});
